bfaeb482c841cedd671a279beb7aecd5483c483d
SayedAbdullahSayedi <sayedabdullahsayedi@gmail.com>
2024-10-17 01:43:35 +0430

    Add files via upload

---
+++ 1 Task_One.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int Numbers = 2024;
+    float Decimal_Numbers = 2.34;
+    char Alphabetic_Letters = 'O';
+    bool TrueAndFalse = false;
+
+    cout << "This is an integer: " << Numbers << endl;
+    cout << "This is a decimal: " << Decimal_Numbers << endl;
+    cout << "This is a letter: " << Alphabetic_Letters << endl;
+    cout << "This is a boolean: " << TrueAndFalse << endl;
+    return 0;
+}
